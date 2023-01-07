6621a3f75782ac29f680f19b13ea998ba9256df7
Lucas A <83725986+smokeeaasd@users.noreply.github.com>
2023-01-07 00:14:13 -0300

    Delete anytests.js

--- tests/anytests.js
+++
@@ -1,9 +0,0 @@
-const { UserValidator } = require("../app/commands/commandUtils/UserValidator.js");
-const { Model } = require("../app/database/model/dbModel.js");
-
-let id = "637679977361309715";
-
-const player = Model.getUserByID(id);
-
-console.log(player);
-console.log(UserValidator.isRegistered(id));
